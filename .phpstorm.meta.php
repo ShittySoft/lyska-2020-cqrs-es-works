15aa41e4115aa3fe93a6ec7a197bcbabb4557a41
Marco Pivetta <ocramius@gmail.com>
2019-12-07 12:35:46 +0100

    Better static analysis/intellisense for PSR containers

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,9 +2,15 @@
 
 namespace PHPSTORM_META
 {
-
     override(
         \Interop\Container\ContainerInterface::get(0),
+        map([
+            "" == "@",
+        ])
+    );
+
+    override(
+        \Psr\Container\ContainerInterface::get(0),
         map([
             "" == "@",
         ])
