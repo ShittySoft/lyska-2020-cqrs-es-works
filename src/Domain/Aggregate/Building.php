a1623dd8b9a6198c02b90a583a28c45e13bd1fd6
Marco Pivetta <ocramius@gmail.com>
2019-12-07 08:42:16 +0100

    Marked `whenNewBuildingWasRegistered` as `protected` for better clarity

--- src/Domain/Aggregate/Building.php
+++ src/Domain/Aggregate/Building.php
@@ -44,7 +44,7 @@ final class Building extends AggregateRoot
         // @TODO to be implemented
     }
 
-    public function whenNewBuildingWasRegistered(NewBuildingWasRegistered $event)
+    protected function whenNewBuildingWasRegistered(NewBuildingWasRegistered $event)
     {
         $this->uuid = Uuid::fromString($event->aggregateId());
         $this->name = $event->name();
